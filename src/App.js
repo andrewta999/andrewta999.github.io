902c548d46cad8e9287f2257e980760a1e306e37
andrewta999 <andrewta365@gmail.com>
2020-12-29 16:32:44 -0500

    initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import 'bootstrap/dist/js/bootstrap.min.css'
+import './css/style.css'
+
+import NavBar from './main/NavBar'
+import Main from './main/Main'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-Light">
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-3 col-sm-12">
+
+                    </div>
+
+                    <div className="col-9 col-sm-12">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
