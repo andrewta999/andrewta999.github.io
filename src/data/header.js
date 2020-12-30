17bddd0ceed6ca5962b9ca54d188ac9b3c1dede9
andrewta999 <andrewta365@gmail.com>
2020-12-30 00:20:45 -0500

    added data for header components

---
+++ src/data/header.js
@@ -0,0 +1,32 @@
+let header_data = {
+    "projects": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "work": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "about": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "education": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "leadership": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "achievements": {
+        "title": "Projects",
+        "body": "Projects"
+    },
+    "contacts": {
+        "title": "Projects",
+        "body": "Projects"
+    }
+}
+
+export default header_data
