55589a79a5f8c5534dcb8c3a6aadd70473e58878
andrewta999 <andrewta365@gmail.com>
2020-12-30 00:21:22 -0500

    defined the main layout for the website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+} from "react-router-dom";
 
-import 'bootstrap/dist/js/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/style.css'
 
 import NavBar from './main/NavBar'
@@ -8,21 +11,23 @@ import Main from './main/Main'
 
 function App() {
     return (
-        <div className="App-Light">
-            <div className="container">
-                <div className="row">
+        <Router>
+            <div className="App-Light" id="App">
+                <div className="container-fluid">
+                    <div className="row">
 
-                    <div className="col-3 col-sm-12">
+                        <div className="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3">
+                            <NavBar />
+                        </div>
+
+                        <div className="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
+                            <Main />
+                        </div>
 
                     </div>
-
-                    <div className="col-9 col-sm-12">
-
-                    </div>
-
                 </div>
             </div>
-        </div>
+        </Router>
     )
 }
 
