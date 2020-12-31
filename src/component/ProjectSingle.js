8996773f33df31f77854577caaf253ac6018f7bc
andrewta999 <andrewta365@gmail.com>
2020-12-31 13:46:20 -0500

    added ProjectSingle component

---
+++ src/component/ProjectSingle.js
@@ -0,0 +1,33 @@
+export default function ProjectSingle(props) {
+    let { data, name } = props
+    let { date, text, image, tech, company } = data
+
+    let tech_list = tech.map((name) => {
+        return <span className="badge text-light ProjectSingle-badge" key={name}>{name}</span>
+    })
+
+    return <div className="container">
+        <div className="ProjectSingle row">
+
+            <div className="col-12 col-md-7 col-ls-7 col-xl-7">
+                <div className="ProjectSingle-name">
+                    {name}
+                    <span className="ProjectSingle-subname">
+                        {` ${company} - ${date}`}
+                    </span>
+                </div>
+                <div className="ProjectSingle-text">
+                    {text}
+                </div>
+                <div className="ProjectSingle-tech">
+                    {tech_list}
+                </div>
+            </div>
+            <div className="col-12 col-md-5 col-ls-5 col-xl-5 Project-img-container">
+                <img src={image} alt="" className="ProjectSingle-img"></img>
+            </div>
+
+        </div>
+    </div>
+
+}
