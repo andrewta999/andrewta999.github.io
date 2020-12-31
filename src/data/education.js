e928533e8cc588ba39ea6f9b9c729cc5cc99d858
andrewta999 <andrewta365@gmail.com>
2020-12-31 14:32:57 -0500

    added education data

---
+++ src/data/education.js
@@ -0,0 +1,22 @@
+import Mcgill from '../img/mcgill.png'
+
+let education = {
+    "Mcgill University": {
+        "location": "@Montreal-Canada | Sept 2018 - May 2022",
+        "degree": "Bachelor of Software Engineering | CGPA: 3.94/4.0",
+        "course": [
+            "COMP 251: Data structures and Algorithms",
+            "COMP 302: Programming Paradigm",
+            "ECSE 324: Computer Organization",
+            "COMP 360: Algorithms Design",
+            "COMP 330: Theory of Computation",
+            "ECSE 321: Intro to Software Engineering",
+            "COMP 424: Artificial Intelligence",
+            "COMP 421: Database Systems",
+            "ECSE 428: Software Engineering Practice"
+        ],
+        "logo": Mcgill
+    }
+}
+
+export default education
