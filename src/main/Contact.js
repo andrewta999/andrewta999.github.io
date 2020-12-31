1f8ce53991cf15aac8ae7b7de4b3b3008078d60f
andrewta999 <andrewta365@gmail.com>
2020-12-30 23:29:15 -0500

    added Contact component

---
+++ src/main/Contact.js
@@ -0,0 +1,8 @@
+import contacts from '../data/contact'
+import Table from '../component/Table'
+
+export default function Contact() {
+    return <div className="Contact">
+        <Table data={contacts}/>
+    </div>
+}
