60323ab70701e07d17c07d977f005d016b26eca2
andrewta999 <andrewta365@gmail.com>
2020-12-31 14:01:39 -0500

    added github link

--- src/data/project.js
+++ src/data/project.js
@@ -11,42 +11,48 @@ let projects = {
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": Whatsnew365
+        "image": Whatsnew365,
+        "github": "https://github.com/andrewta999/Whatsnew365"
     },
     "SpaceTime": {
         "company": "@project",
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": SpaceTime  
+        "image": SpaceTime,
+        "github": "https://github.com/andrewta999/SpaceTime"  
     },
     "OSA app": {
         "company": "@project",
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": OSAapp 
+        "image": OSAapp,
+        "github": "https://github.com/andrewta999/OSAapp" 
     },
     "Pet App": {
         "company": "@project",
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": PetApp 
+        "image": PetApp,
+        "github": "https://github.com/andrewta999/Whatsnew365" 
     },
     "Quoridor App": {
         "company": "@project",
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": Quoridor 
+        "image": Quoridor,
+        "github": "https://github.com/McGill-ECSE223-Fall2019/ecse223-project--group-10" 
     },
     "Movie Recommander": {
         "company": "@project",
         "date": "Fall 2020",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
-        "image": MovieApp  
+        "image": MovieApp,
+        "github": "https://github.com/andrewta999/Movie_Recommender"  
     },
 }
 
