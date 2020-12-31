a92cff95006011b275a3ae14d67f3fa0befc6b2b
andrewta999 <andrewta365@gmail.com>
2020-12-30 23:32:13 -0500

    improved number readability

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,6 @@
 let contacts = {
     "Email": "andrew.ta@mail.mcgill.ca", 
-    "Phone": "4388283417",
+    "Phone": "438-828-3417",
     "Address": "Montreal, QC, Canada", 
     "Linkedin": <a href="https://www.linkedin.com/in/andrewta99" target="_blank" rel="noreferrer">Linkedin</a>,
     "Twitter": <a href="https://twitter.com/AndrewTa99" target="_blank" rel="noreferrer">Twitter</a>
