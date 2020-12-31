a71c2e7803fa128c32be9cbdfeb93e653a40c2c5
andrewta999 <andrewta365@gmail.com>
2020-12-31 13:46:34 -0500

    added dummy project data

---
+++ src/data/project.js
@@ -0,0 +1,53 @@
+import Whatsnew365 from '../img/whatsnew365.png'
+import SpaceTime from '../img/spacetime.png'
+import MovieApp from '../img/movie_recommender.png'
+import OSAapp from '../img/osa_app.png'
+import PetApp from '../img/pet_app.png'
+import Quoridor from '../img/quoridor_app.png'
+
+let projects = {
+    "Whatsnew365": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": Whatsnew365
+    },
+    "SpaceTime": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": SpaceTime  
+    },
+    "OSA app": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": OSAapp 
+    },
+    "Pet App": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": PetApp 
+    },
+    "Quoridor App": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": Quoridor 
+    },
+    "Movie Recommander": {
+        "company": "@project",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["JavaScript", "Node.js", "HTML", "CSS", "React", "GIT", "Express", "Docker", "Kubernetes"],
+        "image": MovieApp  
+    },
+}
+
+export default projects
