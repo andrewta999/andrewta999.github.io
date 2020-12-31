910a38003f00bf2fe4028d42b0924c53b27f375e
andrewta999 <andrewta365@gmail.com>
2020-12-31 15:00:50 -0500

    added leadership data

---
+++ src/data/leadership.js
@@ -0,0 +1,19 @@
+import Robotics from '../img/mcgill_robotics.png'
+import AI from '../img/mcgill_ai.jpg'
+
+let leadership = {
+    "Mcgill Robotics": {
+        "location": "Mcgill",
+        "date": "2019-2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "logo": Robotics
+    },
+    "Mcgill AI": {
+        "location": "Mcgill",
+        "date": "2018-2019",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "logo": AI
+    }
+}
+
+export default leadership
