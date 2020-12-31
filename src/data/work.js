59bd37bffee66fd301b6e8a02c104cc802942417
andrewta999 <andrewta365@gmail.com>
2020-12-31 16:54:36 -0500

    added data

---
+++ src/data/work.js
@@ -0,0 +1,14 @@
+import Ciena from '../img/ciena.jpg'
+
+let work = {
+    "Full Stack Developer Intern": {
+        "company": "@Ciena",
+        "date": "Fall 2020",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tech": ["Python", "Node.js", "JavaScript", "HTML", "CSS", "React", "GIT", "Docker", "Kubernetes"],
+        "image": Ciena,
+        "github": "https://github.com/andrewta999/Whatsnew365"
+    },
+}
+
+export default work
