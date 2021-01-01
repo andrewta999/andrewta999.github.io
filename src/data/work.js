51cca1ef25c64fd44a4ac765b83c2b68168242c5
andrewta999 <andrewta365@gmail.com>
2021-01-01 13:51:52 -0500

    fixed a typo

--- src/data/work.js
+++ src/data/work.js
@@ -4,7 +4,7 @@ let work = {
     "Full Stack Developer Intern": {
         "company": "@Ciena",
         "date": "Fall 2020",
-        "text": "I had ownership of a web application that controled lab instruments at Ciena. This project replaced the need of a third-pary instrument-control softare that costed the company hundreds of thousands of dollars per year. I was in charged of both building the backend (a Rest API) in Python that talked to the instruments and a web browser interface in JavaScript and HTML that comminucated with the backend to fetch and stream data from the instruments. I used PostgreSQL to store the data. The project was then deployed with Docker and Kubernetes.",
+        "text": "I had ownership of a web application that controled lab instruments at Ciena. This project replaced the need of a third-party instrument-control softare that costed the company hundreds of thousands of dollars per year. I was in charged of both building the backend (a Rest API) in Python that talked to the instruments and a web browser interface in JavaScript and HTML that comminucated with the backend to fetch and stream data from the instruments. I used PostgreSQL to store the data. The project was then deployed with Docker and Kubernetes.",
         "tech": ["Python", "Node.js", "JavaScript", "HTML", "CSS", "React", "PostgreSQL", "Docker", "Kubernetes"],
         "image": Ciena,
         "github": "https://github.com/andrewta999/Whatsnew365"
