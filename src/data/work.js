1fbcdf702ec11da2b75c15f80d0c7215a2cc5967
andrewta999 <andrewta365@gmail.com>
2021-01-09 13:53:46 -0500

    modified

--- src/data/work.js
+++ src/data/work.js
@@ -4,7 +4,7 @@ let work = {
     "Full Stack Developer Intern": {
         "company": "@Ciena",
         "date": "Fall 2020",
-        "text": "I developed a web application that controlled and streamed data from lab instruments at Ciena. This project removed the cost of hundreds of thousands of dollars per year spent on third-party software. I was in charged of both building the backend (a Rest API) in Python that connected to the instruments and a web browser interface in JavaScript and HTML that communicated with the backend. I used PostgreSQL to store the data from the instruments. The project was deployed with Docker and Kubernetes.",
+        "text": "I used Python and JavaScript to develop a web application that controlled and streamed data from lab instruments at Ciena. This project aided my team of scientists and engineers in their researches and removed the need of a third-party software that costed 100 thousands dollars per year. I was in charged of both building the backend (a Rest API) that connected to the instruments and a web browser interface that communicated with the backend to display data to users. I used PostgreSQL to store the data from the instruments. The project was deployed with Docker and Kubernetes.",
         "tech": ["Python", "Node.js", "JavaScript", "HTML", "CSS", "React", "PostgreSQL", "Docker", "Kubernetes"],
         "image": Ciena,
         "github": "https://github.com/andrewta999/Whatsnew365"
